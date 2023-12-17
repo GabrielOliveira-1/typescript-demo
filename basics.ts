0145b76d64e2a8a84a154d9a647ee09b33e7a0fa
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2023-12-17 14:20:29 -0700

    Create basics.ts
    - Practicing arrays and object types in typescript

---
+++ basics.ts
@@ -0,0 +1,42 @@
+// Primitives: number, string, boolean
+// Complex types: arrays, objects
+// Function types, parameters
+
+// Primitives
+
+let age: number = 24;
+
+age = 12;
+
+let firstName: string;
+
+firstName = "Gabriel";
+
+let isDeveloper: boolean;
+
+isDeveloper = true;
+
+// Complex types
+
+let hobbies: string[];
+
+hobbies = ["Games", "Cooking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Gab",
+  age: 26,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: {
+  name: string;
+  age: number;
+}[];
